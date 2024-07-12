08c4bdb812c4b918ed9aca9a943db001d806f24e
Anemaygi <gianemayumisan@gmail.com>
2024-07-12 15:29:33 +0800

    feat: Ranking section on landing page using ranking component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,17 @@ export default function Home() {
   return (
     <main>
       {/* Add components from Landing Page here */}
-      <Ranking equipes={equipes} />
+
+      {/* Ranking section using ranking component */}
+      <section className="flex flex-col my-12 lg:px-16 gap-12">
+        <h1 className="text-7xl px-8">Ranking</h1>
+        <Ranking equipes={equipes} displayQuantity={5}/>
+        <div className="flex justify-center w-full">
+          <button className="border w-fit border-green-500">botao p pag de ranking</button>
+        </div>
+      </section>
+
+
     </main>
   );
 }
