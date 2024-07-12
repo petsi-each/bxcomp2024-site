312e06e6d2e1bdb012615b252af31096a49fa703
Anemaygi <gianemayumisan@gmail.com>
2024-07-12 12:38:46 +0800

    style: add bubblegum sans to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Bubblegum_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const bubblegum = Bubblegum_Sans(
+  {
+    subsets: ["latin"],
+    weight: "400"
+  }
+);
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={bubblegum.className}>{children}</body>
     </html>
   );
 }
