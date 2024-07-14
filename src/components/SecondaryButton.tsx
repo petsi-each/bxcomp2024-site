f34d93086886b2b34c819438d1ebb54996c3f24e
thais-fujissaka <80991615+thais-fujissaka@users.noreply.github.com>
2024-07-13 23:05:24 -0300

    style: add hover and active styles to primary and secondary buttons

--- src/components/SecondaryButton.tsx
+++ src/components/SecondaryButton.tsx
@@ -9,7 +9,7 @@ export default function SecondaryButton(props: SecondaryButtonProps) {
 
     return(
 
-        <button onClick={props.onClick} className="w-auto bg-white/40 text-verdeBX border-2 border-verdeBX rounded-full px-14 py-3.5 text-xl">
+        <button onClick={props.onClick} className="w-auto bg-white/40 text-verdeBX border-2 border-verdeBX rounded-full px-14 py-3.5 text-xl hover:bg-[#369434] hover:text-white hover:border-[#369434] active:translate-y-0.5">
             
             {props.title}
             
