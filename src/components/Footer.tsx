40d252fc7d056ba9adb262147d29235ad23f2a4a
Fernando Toba Lopez <fetobalopez@gmail.com>
2024-07-16 18:33:53 -0300

    Chore: Adiciona waves.svg ao footerr

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-    <footer className="bg-transparent relative flex flex-row flex-nowrap w-full h-56 mt-10 items-end justify-between">
+    <footer className="bg-transparent relative flex flex-row flex-nowrap w-full h-80 mt-10 items-end justify-between before:content-[''] before:absolute before:bg-cover before:w-full before:h-80 before:bg-[url('/waves.svg')] before:bg-no-repeat before:bg-top before:-z-10">
         <h1 className="flex-1 text-center">Desenvolvido Por PET-SI 2024</h1>
         
         <i className="flex-1 text-center">Logo Coruja</i>
