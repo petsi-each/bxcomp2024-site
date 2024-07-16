39d0dc1f97a9312750fde699f99d98fa7b8885e5
Fernando Toba Lopez <fetobalopez@gmail.com>
2024-07-16 19:56:42 -0300

    implementa logo do pet-Bx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
+
+
 export default function Footer() {
     return (
     <footer className="bg-transparent relative flex flex-row flex-nowrap w-full h-80 mt-10 items-end justify-between before:content-[''] before:absolute before:bg-cover before:w-full before:h-80 before:bg-[url('/waves.svg')] before:bg-no-repeat before:bg-top before:-z-10">
-        <h1 className="flex-1 text-center">Desenvolvido Por PET-SI 2024</h1>
+        <h1 className="flex-1 text-center font-sans text-black">Desenvolvido Por PET-SI 2024</h1>
         
-        <i className="flex-1 text-center">Logo Coruja</i>
+        <img src = "logoCoruja.svg" alt=" "/>
         
-        <ul className="flex-1 justify-center text-center">
+        <ul className="flex-1 justify-center flex-row text-center">
            <i>Facebook</i>
            <i>Linkedind</i>
            <i>email</i>
-           <i>Instagram</i>
+           <i >Instagram</i>
+           
         </ul>
     
     </footer>        
