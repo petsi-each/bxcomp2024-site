38ab7d34d47862666b57bb3bd17f5483a5d7c6ce
Fernando Toba Lopez <fetobalopez@gmail.com>
2024-07-16 21:03:05 -0300

    fix: Altera forma de disposição de elementos para grid

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 
 export default function Footer() {
     return (
-    <footer className="bg-transparent relative flex flex-row flex-nowrap w-full h-80 mt-10 items-end justify-between before:content-[''] before:absolute before:bg-cover before:w-full before:h-80 before:bg-[url('/waves.svg')] before:bg-no-repeat before:bg-top before:-z-10">
-        <h1 className="flex-1 text-center font-sans text-black">Desenvolvido Por PET-SI 2024</h1>
+    <footer className=" grid grid-cols-3 grid-rows-1 gap-3 bg-transparent relative w-full h-80 mt-10 items-end justify-between before:content-[''] before:absolute before:bg-cover before:w-full before:h-80 before:bg-[url('/waves.svg')] before:bg-no-repeat before:bg-top before:-z-10">
+        <h1 className="pb-8 flex-1 text-center font-sans text-black">Desenvolvido Por PET-SI 2024</h1>
         
+        <div className="pb-4 flex items-center justify-center" >
         <img src = "logoCoruja.svg" alt=" "/>
-        
-        <ul className="flex-1 justify-center flex-row text-center">
+        </div>
+        <ul className="pb-8 justify-center flex-row text-center">
            <i>Facebook</i>
            <i>Linkedind</i>
            <i>email</i>
