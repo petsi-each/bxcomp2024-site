c8e81dc26c5e43a7413e98ed0daa7ed4a266368e
segalv <anaclarasvp@usp.br>
2024-07-17 13:03:40 -0300

    Update ao json

--- src/app/EtapasDesafios/page.tsx
+++ src/app/EtapasDesafios/page.tsx
@@ -6,35 +6,31 @@ import { desafioList } from './components/CardDesafios';
 import { useState } from "react";
 
 interface etapa {
-    etapa: number,
+    etapaIndice: number,
     estado: boolean,
     tema: string,
     icon: string,
     altIcon: string,
     desafios: desafioList
 }
-//testando
-{/*interface etapaList {
-    etapas: etapa[]
-}
-*/}
+
 export default function EtapasDesafios() {
-    const props: etapa[] = etapasDesafios as unknown as etapa[]; //check 
+    etapasDesafios as unknown as etapa[]; 
     const [indice, setIndice] = useState(-1);
     return (
         <main>
             <h1 className="px-60 py-32 text-right text-brancoBX text-6xl"> Etapas <br></br> <span className="text-laranjaBX">&</span> Desafios</h1> {/*whitespace-pre-line não funcionou*/}
             <div className="grid grid-cols-5 gap-x-5">
                 {
-                    props.map((etapa) => (
-                        <button onClick={() => setIndice(etapa.etapa)} disabled={etapa.estado == false} title={'Etapa ${etapa.etapa'} className='flex justify-center size-48 rounded-3xl'> {/*definir função para onClick, para que que um segundo clique recolha os balões de desafios*/}
+                    etapasDesafios.map((etapa) => (
+                        <button onClick={() => setIndice(etapa.etapaIndice)} disabled={etapa.estado == false} title={'Etapa ${etapa.etapaIndice'} className='flex justify-center size-48 rounded-3xl'> {/*definir função para onClick, para que que um segundo clique recolha os balões de desafios*/}
                             {
                                 etapa.estado == false ?
                                     <ImageG className="size-fit rounded-3xl" src="/staticTV.gif" alt="TV estática" width="2" height="2" />
                                     :
                                     <div>
                                         <ImageG className="size-fit rounded-3xl opacity-80" src={etapa.icon} alt={etapa.altIcon} width="2" height="2" />
-                                        <h2 className="mt-32 p-1.5 font-Poppins text-white text-2xl"><b>Etapa {etapa.etapa}</b><br></br></h2> {/*resolver a situação do sublinhado; tem que checar se esse break aq vai do the trick*/}
+                                        <h2 className="mt-32 p-1.5 font-Poppins text-white text-2xl"><b>Etapa {etapa.etapaIndice}</b><br></br></h2> {/*resolver a situação do sublinhado; tem que checar se esse break aq vai do the trick*/}
                                         <h3 className="font-Poppins text-white text-base">{etapa.tema}</h3>
                                     </div>
                             }
