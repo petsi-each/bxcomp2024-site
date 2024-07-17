fdb5157aee07790765e8eb902b525a933839a784
segalv <anaclarasvp@usp.br>
2024-07-17 13:53:20 -0300

    Deleção interface etapa

--- src/app/EtapasDesafios/components/CardDesafios.tsx
+++ src/app/EtapasDesafios/components/CardDesafios.tsx
@@ -12,12 +12,12 @@ export interface desafioList {
 
 export default function CardDesafios(props: desafioList) {
     return (
-        <div className="grid grid-cols-1 gap-y-8 py-32">
+        <div className="grid grid-cols-1 gap-y-8 py-32 flex justify-center">
             {
                 props.desafios.map((desafio) => (
                     <article className="flex-col w-[500px] h-28 bg-[#FBF3AD] rounded-e-full text-center text-pretoBX">
                         <h3 className="text-2xl">{desafio.desafio}</h3>
-                        <p className="font-Poppins text-base">
+                        <p className="font-Poppins text-base flex-col">
                             <a href={desafio.enunciado} target="_blank">Enunciado</a>
                             <a href={desafio.testes} target="_blank">Casos Teste</a>
                             <a href={desafio.resolucao} target="_blank">Exemplo de Gabarito</a>
--- src/app/EtapasDesafios/page.tsx
+++ src/app/EtapasDesafios/page.tsx
@@ -5,28 +5,23 @@ import CardDesafios from './components/CardDesafios';
 import { desafioList } from './components/CardDesafios';
 import { useState } from "react";
 
-interface etapa {
-    etapaIndice: number,
-    estado: boolean,
-    tema: string,
-    icon: string,
-    altIcon: string,
-    desafios: desafioList
-}
+
 
 export default function EtapasDesafios() {
-    etapasDesafios as unknown as etapa[];
     const [indice, setIndice] = useState(-1);
     return (
         <main>
             <h1 className="px-60 py-32 text-right text-brancoBX text-6xl"> Etapas <br></br> <span className="text-laranjaBX">&</span> Desafios</h1> {/*whitespace-pre-line não funcionou*/}
+            
             <div className="grid grid-cols-5 gap-x-5">
                 {
                     etapasDesafios.map((etapa) => (
                         <button onClick={() => setIndice(etapa.etapaIndice)} disabled={etapa.estado == false} title={'Etapa ${etapa.etapaIndice'} className='flex justify-center size-48 rounded-3xl'> {/*definir função para onClick, para que que um segundo clique recolha os balões de desafios*/}
                             {
                                 etapa.estado == false ?
-                                    <ImageG className="size-fit rounded-3xl" src="/staticTV.gif" alt="TV estática" width="2" height="2" />
+                                    <div>
+                                        <ImageG className="size-fit rounded-3xl" src="/staticTV.gif" alt="TV estática" width="2" height="2" />
+                                    </div>
                                     :
                                     <div>
                                         <ImageG className="size-fit rounded-3xl opacity-80" src={etapa.icon} alt={etapa.altIcon} width="2" height="2" />
