5d2331e2514dce7ff1497ad866506e7490ed1847
segalv <anaclarasvp@usp.br>
2024-07-19 16:14:03 -0300

    Adição do uso de scale ao clique e mudança do destaque dado à etapa em foco

--- src/app/EtapasDesafios/page.tsx
+++ src/app/EtapasDesafios/page.tsx
@@ -25,7 +25,7 @@ export default function EtapasDesafios() {
                     {
                         etapasDesafios.map((etapa) => (
                             <button onClick={() => handleOnClick(etapa.etapaIndice)} disabled={etapa.estado == false} title={`Etapa ${etapa.etapaIndice}`} 
-                            className={`relative size-48 rounded-3xl ${etapa.estado == true ? 'hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:underline hover:decoration-verdeBX hover:bg-blue-600 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''} ${indice == etapa.etapaIndice ? 'size-56 ring-4 ring-azulBX ring-opacity-30' : ''}`}> 
+                            className={`relative size-48 rounded-3xl ${etapa.estado == true ? 'hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:underline hover:decoration-verdeBX hover:bg-blue-600 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''} ${indice == etapa.etapaIndice ? 'scale-110 shadow-sky-900 shadow-lg ' : ''} ${indice != -1 && indice != etapa.etapaIndice ? 'scale-90':''}`}> 
                              {/*procurar por uma forma mais concisa de usar hover,etc */}
                                 {
                                     etapa.estado == false ?
@@ -35,8 +35,8 @@ export default function EtapasDesafios() {
                                         :
                                         <div>
                                             <ImageG className="rounded-3xl opacity-80" src={etapa.icon} alt={etapa.altIcon} fill={true} />
-                                            <h2 className="font-poppins text-white mt-28 text-2xl"><b>Etapa {etapa.etapaIndice}</b></h2> {/*TODO: o texto está sendo renderizado por baixo da imagem*/}
-                                            <h3 className="-mt-1 font-poppins text-white test-base">{etapa.tema}</h3>
+                                            <h2 className="mt-28 font-poppins text-white text-2xl "><b>Etapa {etapa.etapaIndice}</b></h2> {/*TODO: o texto está sendo renderizado por baixo da imagem*/}
+                                            <h3 className="-mt-1 font-poppins text-white text-base">{etapa.tema}</h3>
                                         </div>
                                 }
                             </button>
