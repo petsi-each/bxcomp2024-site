9510f8e379e099869e6e3c354a97ca31ae7b80a9
jun <gustavonagatomo@gmail.com>
2024-07-19 19:34:31 -0300

    fix: Evita overflow horizontal do botão

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -11,9 +11,9 @@ export default function PrimaryButton(props: PrimaryButtonProps) {
 
     return(
 
-        <button onClick={props.onClick} className="w-auto bg-verdeBX text-white rounded-full px-8 py-4 flex flex-row place-content-center items-center text-xl hover:bg-[#369434] active:translate-y-0.5">
+        <button onClick={props.onClick} className="w-full bg-verdeBX text-white rounded-full px-8 py-4 flex flex-row place-content-center items-center text-xl hover:bg-[#369434] active:translate-y-0.5">
 
-            <PiRocketLaunchDuotone size={28} />
+            <PiRocketLaunchDuotone className="hidden sm:block" size={28} />
 
             <div className="px-4">
                 {props.title}
