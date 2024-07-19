f663fac365368cd018747c9126f6e624604e927a
segalv <anaclarasvp@usp.br>
2024-07-19 11:43:53 -0300

    Correção dos erros de renderização e adição de elementos iterativos

--- src/app/EtapasDesafios/page.tsx
+++ src/app/EtapasDesafios/page.tsx
@@ -2,42 +2,52 @@
 import etapasDesafios from '@/data/etapasDesafios.json';
 import ImageG from '@/components/ImageG';
 import CardDesafios from './components/CardDesafios';
-import { desafioList } from './components/CardDesafios';
 import { useState } from "react";
 
 
-
 export default function EtapasDesafios() {
+
     const [indice, setIndice] = useState(-1);
+
+    const handleOnClick = (etapaIndice: number) =>{
+        indice == etapaIndice ?
+            setIndice(-1)
+            :
+            setIndice(etapaIndice)
+    }
+    
     return (
         <main>
-            <h1 className="px-60 py-32 text-right text-brancoBX text-6xl"> Etapas <br></br> <span className="text-laranjaBX">&</span> Desafios</h1> {/*whitespace-pre-line não funcionou*/}
-            
-            <div className="grid grid-cols-5 gap-x-5">
+            <h1 className="mx-60 mt-32 text-right text-6xl text-brancoBX"> Etapas <br></br> <span className="text-laranjaBX">&</span> Desafios</h1> {/*whitespace-pre-line não funcionou*/}
+            <div className="flex items-center flex-col">
+                <div className="my-32 grid grid-cols-5 gap-x-5 w-fit"> {/*TODO:no clique do botão de cada Etapa o gap está mudando*/}
+                    {
+                        etapasDesafios.map((etapa) => (
+                            <button onClick={() => handleOnClick(etapa.etapaIndice)} disabled={etapa.estado == false} title={'Etapa ${etapa.etapaIndice}'} 
+                            className={`relative size-48 rounded-3xl ${etapa.estado == true ? 'hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:underline hover:decoration-verdeBX hover:decoration-2 hover:bg-blue-600 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''} ${indice == etapa.etapaIndice ? 'size-60 ring-4 ring-azulBX ring-opacity-30' : ''}`}> 
+                             {/*procurar por uma forma mais concisa de usar hover,etc */}
+                                {
+                                    etapa.estado == false ?
+                                        <div>
+                                            <ImageG className="rounded-3xl" src="/staticTV.gif" alt="TV estática" fill={true} />
+                                        </div>
+                                        :
+                                        <div>
+                                            <ImageG className="rounded-3xl opacity-80" src={etapa.icon} alt={etapa.altIcon} fill={true} />
+                                            <h2 className="mt-32 font-poppins text-white text-2xl"><b>Etapa {etapa.etapaIndice}</b></h2> {/*TODO: o texto está sendo renderizado por baixo da imagem*/}
+                                            <h3 className="font-poppins text-white text-base">{etapa.tema}</h3>
+                                        </div>
+                                }
+                            </button>
+                        ))
+                    }
+                </div>
                 {
-                    etapasDesafios.map((etapa) => (
-                        <button onClick={() => setIndice(etapa.etapaIndice)} disabled={etapa.estado == false} title={'Etapa ${etapa.etapaIndice'} className='flex justify-center size-48 rounded-3xl'> {/*definir função para onClick, para que que um segundo clique recolha os balões de desafios*/}
-                            {
-                                etapa.estado == false ?
-                                    <div>
-                                        <ImageG className="size-fit rounded-3xl" src="/staticTV.gif" alt="TV estática" width="2" height="2" />
-                                    </div>
-                                    :
-                                    <div>
-                                        <ImageG className="size-fit rounded-3xl opacity-80" src={etapa.icon} alt={etapa.altIcon} width="2" height="2" />
-                                        <h2 className="mt-32 p-1.5 font-Poppins text-white text-2xl"><b>Etapa {etapa.etapaIndice}</b><br></br></h2> {/*resolver a situação do sublinhado; tem que checar se esse break aq vai do the trick*/}
-                                        <h3 className="font-Poppins text-white text-base">{etapa.tema}</h3>
-                                    </div>
-                            }
-                        </button>
-                    ))
+                    indice != -1 && (
+                        <CardDesafios desafios={(etapasDesafios[indice]).desafios} />
+                    )
                 }
             </div>
-            {
-                indice !== -1 &&(
-                    <CardDesafios desafios={(etapasDesafios[indice]).desafios} />
-                )
-            }
         </main>
     );
 }
