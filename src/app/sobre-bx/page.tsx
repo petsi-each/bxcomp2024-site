fff590baf60c01adf609c2917949c18d3bd833b3
sofialeopoldo83 <sofiafleopoldo@gmail.com>
2024-07-20 12:01:41 -0300

    feat adicionar pagina

---
+++ src/app/sobre-bx/page.tsx
@@ -0,0 +1,15 @@
+export default function o_que_eh_BXComp() {
+  return (
+    <main>
+     
+      <h1>
+        O que é o BXComp?
+      </h1>
+      <p>
+      O BXComp é o tradicional Campeonato de Programação para Calouros do Curso de Sistemas de Informação da EACH-USP. 
+
+      Neste ano, o BXCOMP está em sua 14ª edição com o tema de Desenhos animados!
+      </p>
+    </main>
+  );
+}
