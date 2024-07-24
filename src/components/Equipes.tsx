b4bdebef36da9c4fb208277acc5aca134959ed3e
Daniyutak <dtakara1101@gmail.com>
2024-07-23 21:45:23 -0300

    cria o arquivo do componente equipes

---
+++ src/components/Equipes.tsx
@@ -0,0 +1,18 @@
+import ImageG from "@/components/ImageG"
+import { Poppins } from 'next/font/google'
+import equipes_data from '@/data/landingpage.json'
+
+export default function Equipes(){
+return (
+<section>
+    <section>
+
+    </section>
+
+    <section>
+        <h2>Equipes</h2>
+
+    </section>
+</section>
+
+)}
