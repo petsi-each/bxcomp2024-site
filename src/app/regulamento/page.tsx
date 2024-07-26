9bacfd46958cae54ede3a82fe8f1471093a03526
Anemaygi <gianemayumisan@gmail.com>
2024-07-26 09:57:01 +0800

    feat: general structure of the page

---
+++ src/app/regulamento/page.tsx
@@ -0,0 +1,21 @@
+import { getFileFromPublic } from '@/lib/Files'
+
+export default function Regulamento() {
+
+  return (
+    <main>
+      
+        <h1>Regulamento</h1>
+
+        <p>Última atualização em: 12/06/2024</p>
+
+        <p> Iframe </p>
+
+        <ul>
+            <li>Publicdo em 12/06/2024 </li>
+            <li>Atualizado em 17/06/2024</li>
+        </ul>
+
+    </main>
+  );
+}
