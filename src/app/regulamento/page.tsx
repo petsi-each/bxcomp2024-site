c402c083d1c6ac75447e45fd7a74097af6f9eb75
Anemaygi <gianemayumisan@gmail.com>
2024-07-26 10:03:54 +0800

    style: add class to components

--- src/app/regulamento/page.tsx
+++ src/app/regulamento/page.tsx
@@ -3,19 +3,19 @@ import { getFileFromPublic } from '@/lib/Files'
 export default function Regulamento() {
 
   return (
-    <main>
+    <main className='flex flex-col items-center justify-center min-h-screen pt-12'>
       
-        <h1>Regulamento</h1>
+        <h1 className='text-laranjaBX text-6xl md:text-7xl p-16'>Regulamento</h1>
 
         <p>Última atualização em: 12/06/2024</p>
 
-        <p> Iframe </p>
+        <p className="p-12"> Iframe </p>
 
-        <ul>
-            <li>Publicdo em 12/06/2024 </li>
+        <ul className="flex flex-col items-center">
+            <li>Publicado em 12/06/2024 </li>
             <li>Atualizado em 17/06/2024</li>
         </ul>
-
+        
     </main>
   );
 }
