9fa7b2b142c649ffed60ad5d3f8c275268288823
Anemaygi <gianemayumisan@gmail.com>
2024-07-26 10:21:03 +0800

    fix: ts in getFileFromPublic

--- src/lib/Files.tsx
+++ src/lib/Files.tsx
@@ -1,3 +1,3 @@
-export function getFileFromPublic({ filePath }: { filePath: string }):string{
+export function getFileFromPublic(filePath: string):string{
     return process.env.NODE_ENV === "production" ? '/bxcomp2024-site'+filePath : filePath;
 }
