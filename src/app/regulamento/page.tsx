c139705ab57cd40408f8bb71b72ce8f4b87c41a1
Anemaygi <gianemayumisan@gmail.com>
2024-07-26 10:29:57 +0800

    feat: add iframe

--- src/app/regulamento/page.tsx
+++ src/app/regulamento/page.tsx
@@ -15,7 +15,12 @@ export default function Regulamento() {
 
         <p className={`${poppins.className}`}><b>Última atualização em:</b> 12/06/2024</p>
 
-        <p className="p-12"> Iframe </p>
+        <iframe 
+                src={getFileFromPublic("/regulamento/regulamento.pdf")}
+                title="Regulamento" 
+                className="px-6 md:px-16 py-4 w-full h-[36rem] lg:h-[64rem]"
+                > 
+        </iframe> 
 
         <ul className={`flex flex-col items-center ${poppins.className}`}>
             <li><b>Publicado</b> em 12/06/2024 </li>
