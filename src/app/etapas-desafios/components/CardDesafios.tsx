f740952aea18a9506bab17f56a573b4965f0b2bf
segalv <anaclarasvp@usp.br>
2024-07-26 17:48:34 -0300

    Redefinição dimensões CardDesafios

--- src/app/etapas-desafios/components/CardDesafios.tsx
+++ src/app/etapas-desafios/components/CardDesafios.tsx
@@ -15,7 +15,7 @@ export default function CardDesafios(props: desafioList) {
         <div className="mb-20 grid grid-cols-1 gap-y-8">
             {
                 props.desafios.map((desafio) => (
-                    <article className="p-2 px-14 w-[600px] min-h-32 h-fit bg-[#FF9C46] rounded-full text-center text-pretoBX hover:ring-8 hover:ring-azulBX hover:ring-opacity-20">
+                    <article className="p-2 px-14 max-w-[600px] min-h-32 h-fit bg-[#FF9C46] rounded-full text-center text-pretoBX hover:ring-8 hover:ring-azulBX hover:ring-opacity-20">
                         <h3 className="text-2xl">{desafio.desafio}</h3>
                         <p className="flex items-center flex-col font-poppins text-base underline">
                             <a href={desafio.enunciado} target="_blank" className="w-fit hover:font-extrabold hover:text-white">Enunciado</a>
