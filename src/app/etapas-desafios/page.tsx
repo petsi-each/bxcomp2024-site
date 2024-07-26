68d322479ad119d48a9ae2ca35fd2422fb232a74
segalv <anaclarasvp@usp.br>
2024-07-26 14:24:20 -0300

    Reajustes para melhor visualização dos elementos dos Card-Etapas

--- src/app/etapas-desafios/page.tsx
+++ src/app/etapas-desafios/page.tsx
@@ -25,7 +25,7 @@ export default function EtapasDesafios() {
                     {
                         etapasDesafios.map((etapa) => (
                             <button onClick={() => handleOnClick(etapa.etapaIndice)} disabled={etapa.estado == false} title={`Etapa ${etapa.etapaIndice}`} 
-                            className={`relative size-48 rounded-3xl ${etapa.estado == true ? 'hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:underline hover:decoration-verdeBX hover:bg-blue-600 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''} ${indice == etapa.etapaIndice ? 'scale-110 shadow-sky-900 shadow-lg ' : ''} ${indice != -1 && indice != etapa.etapaIndice ? 'scale-90':''}`}> 
+                            className={`relative size-48 rounded-3xl ${etapa.estado == true ? 'hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:underline hover:underline-offset-2 hover:decoration-verdeBX hover:bg-blue-600 hover:bg-opacity-20 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''} ${indice == etapa.etapaIndice ? 'scale-110 shadow-sky-900 shadow-lg ' : ''} ${indice != -1 && indice != etapa.etapaIndice ? 'scale-90':''}`}> 
                              {/*procurar por uma forma mais concisa de usar hover,etc */}
                                 {
                                     etapa.estado == false ?
@@ -34,9 +34,9 @@ export default function EtapasDesafios() {
                                         </div>
                                         :
                                         <div>
-                                            <ImageG className="-z-10 rounded-3xl opacity-80" src={etapa.icon} alt={etapa.altIcon} fill={true} />
-                                            <h2 className="mt-28 font-poppins text-white text-2xl "><b>Etapa {etapa.etapaIndice}</b></h2> {/*TODO: o texto está sendo renderizado por baixo da imagem*/}
-                                            <h3 className="-mt-1 font-poppins text-white text-base">{etapa.tema}</h3>
+                                            <ImageG className="-z-10 rounded-3xl" src={etapa.icon} alt={etapa.altIcon} fill={true} />
+                                            <h2 className="mt-28 font-poppins text-white text-2xl drop-shadow-lg"><b>Etapa {etapa.etapaIndice}</b></h2> {/*TODO: o texto está sendo renderizado por baixo da imagem*/}
+                                            <h3 className="-mt-1 font-poppins text-white text-base drop-shadow-lg">{etapa.tema}</h3>
                                         </div>
                                 }
                             </button>
