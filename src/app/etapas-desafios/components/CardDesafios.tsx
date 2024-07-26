4f103fd68160626158e4da066b95f2ad0926a1bd
segalv <anaclarasvp@usp.br>
2024-07-26 17:14:16 -0300

    Reajuste height CardDesafio

--- src/app/etapas-desafios/components/CardDesafios.tsx
+++ src/app/etapas-desafios/components/CardDesafios.tsx
@@ -15,7 +15,7 @@ export default function CardDesafios(props: desafioList) {
         <div className="mb-20 grid grid-cols-1 gap-y-8">
             {
                 props.desafios.map((desafio) => (
-                    <article className="p-2 w-[600px] h-32 bg-[#FF9C46] rounded-full text-center text-pretoBX hover:ring-8 hover:ring-azulBX hover:ring-opacity-20">
+                    <article className="p-2 w-[600px] min-h-32 h-fit bg-[#FF9C46] rounded-full text-center text-pretoBX hover:ring-8 hover:ring-azulBX hover:ring-opacity-20">
                         <h3 className="text-2xl">{desafio.desafio}</h3>
                         <p className="flex flex-col font-poppins text-base underline">
                             <a href={desafio.enunciado} target="_blank" className="hover:font-extrabold hover:text-white">Enunciado</a>
