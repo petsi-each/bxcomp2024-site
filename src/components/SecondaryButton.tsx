e221ca3a1c993c55440d9f59d0be60121214eba0
thais-fujissaka <80991615+thais-fujissaka@users.noreply.github.com>
2024-07-27 23:32:01 -0300

    fix: Adiciona title aos botões primario e secundário.

--- src/components/SecondaryButton.tsx
+++ src/components/SecondaryButton.tsx
@@ -13,7 +13,7 @@ export default function SecondaryButton(props: SecondaryButtonProps) {
 
     return(
 
-        <button id={buttonId} aria-labelledby={buttonId} onClick={props.onClick} className="w-auto bg-white/40 text-verdeBX border-2 border-verdeBX rounded-full px-12 py-4 text-xl hover:bg-[#369434] hover:text-white hover:border-[#369434] active:translate-y-0.5">
+        <button title={props.title} id={buttonId} aria-labelledby={buttonId} onClick={props.onClick} className="w-auto bg-white/40 text-verdeBX border-2 border-verdeBX rounded-full px-12 py-4 text-xl hover:bg-[#369434] hover:text-white hover:border-[#369434] active:translate-y-0.5">
             
             {props.title}
             
