b30e313913c63cb64d2060f3242ab08b7c106357
Daniyutak <dtakara1101@gmail.com>
2024-07-29 22:21:09 -0300

    ajusta o tamanho do botao equipe

--- src/components/BotaoEquipe.tsx
+++ src/components/BotaoEquipe.tsx
@@ -12,7 +12,7 @@ interface BotaoEquipeProps{
 export default function BotaoEquipe(props: BotaoEquipeProps){
     return(
         
-        <button className="opacity-50 px-4 py-4 mx-1 my-2 rounded-md border border-slate-500 bg-brancoBX focus:bg-laranjaBX focus:opacity-100">
+        <button className="w-20 h-28 opacity-50 px-4 py-4 mx-1 my-2 rounded-md border border-slate-500 bg-brancoBX focus:bg-laranjaBX focus:opacity-100 focus:drop-shadow-sm">
             <ImageG src={props.srcImage} width={32} height={32} alt={`ícone da equipe ${props.nomeEquipe}`}></ImageG>
             <div className="text-pretoBX pt-2">
                 {props.nomeEquipe}
