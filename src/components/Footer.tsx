2eee7abc8959a136ab847409fc546c7ba24b86ac
Fernando Toba Lopez <fetobalopez@gmail.com>
2024-07-30 16:05:44 -0300

    perf: Utiliza, flex-order para mostrar os elementos, ao inves de hidden, colapse e visible

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import { BiLogoFacebookSquare, BiSolidEnvelope, BiLogoLinkedinSquare, BiLogoInstagramAlt } from "react-icons/bi";
+import { BiLogoFacebookSquare, BiSolidEnvelope, BiLogoLinkedinSquare, BiLogoInstagramAlt } from 'react-icons/bi';
 import ImageG from './ImageG';
-import SocialButton from './SocialButton';
+import { IconType } from 'react-icons';
+
+interface SocialButtonProps {
+  href: string;
+  icon: IconType;
+  size: number;
+  color: string;
+  className?: string;
+}
+
+const SocialButton: React.FC<SocialButtonProps> = ({ href, icon: Icon, size, color, className = '' }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`flex items-center ${className}`}
+    >
+      <Icon size={size} color={color} />
+    </a>
+  );
+};
 
 export default function Footer() {
   return (
     <footer className="relative w-full h-80 pt-10 bg-transparent flex-col flex md:flex-row items-center md:items-end justify-end md:justify-between bg-[url('/waves.svg')] bg-cover bg-no-repeat bg-top">
       
       {/* Texto Desktop */}
-      <h1 className="flex collapse md:visible md:h-4 items-center text-center font-sans font-semibold text-black m-5 md:m-10">
+      <h1 className="flex order-last items-center font-medium text-xs text-center text-black m-5 font-sans md:text-sm md:font-semibold md:order-first md:h-4 md:m-10 md:mb-11">
         Desenvolvido por PET-SI 2024
       </h1>
-      
+
       {/* Logo PET e BX Desktop */}
-      <div className="flex collapse md:visible md:h-4 items-center justify-center md:m-10">
-        <ImageG src="/logoCoruja.png" alt="Logo PET" width="140" height="4" />
+      <div className="flex relative items-center justify-center w-24 h-8 m-1 order-first md:h-14 md:w-44 md:m-10 md:mb-6">
+        <ImageG src="/logoCoruja.png" alt="Logo PET" fill={true} />
       </div>
       
       {/* Botões interativos Desktop */}
-      <ul className="flex collapse md:visible md:h-4 space-x-2 md:m-11">
+      <ul className="flex collapse md:visible md:h-4 space-x-2 md:m-10 md:mb-12">
         <li>
           <SocialButton
             href="https://www.facebook.com/petsieach"
@@ -52,11 +73,6 @@ export default function Footer() {
           />
         </li>
       </ul>
-
-      {/* Logo PET e BX mobile */}
-      <div className="flex items-center visible md:hidden justify-center m-2">
-        <ImageG src="/logoCoruja.png" alt="Logo PET" width="100" height="4" />
-      </div>
       
       {/* Botões interativos Mobile */}
       <ul className="flex visible md:hidden md:h-4 space-x-2 m-2">
@@ -94,10 +110,6 @@ export default function Footer() {
         </li>
       </ul>
 
-      {/* Texto Mobile */}
-      <h1 className="flex md:hidden items-center text-center font-medium font-sans text-xs text-black m-5 md:m-10">
-        Desenvolvido por PET-SI 2024
-      </h1>
     </footer>
   );
 }
