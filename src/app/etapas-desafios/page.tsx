a1d5f9f26dc54c4b44b66eb360dc9e891eb38b89
segalv <anaclarasvp@usp.br>
2024-08-05 14:41:40 -0300

    Adição de comentários

--- src/app/etapas-desafios/page.tsx
+++ src/app/etapas-desafios/page.tsx
@@ -12,11 +12,11 @@ const poppins = Poppins({
 
 export default function EtapasDesafios() {
 
-    const [indice, setIndice] = useState(-1);
+    const [indice, setIndice] = useState(-1); 
 
     const handleOnClick = (etapaIndice: number) => {
         if (indice == etapaIndice){
-            setIndice(-1)
+            setIndice(-1) //garante que o segundo clique em um card selecionado recolha os CardDesafios
         } else {
             setIndice(etapaIndice)
             setTimeout(() => { 
@@ -29,20 +29,19 @@ export default function EtapasDesafios() {
 
     return (
         <main>
-            <h1 className="mx-60 mt-20 mb-8 text-right text-5xl text-brancoBX"> Etapas <br></br> <span className="text-laranjaBX">&</span> Desafios</h1> {/*whitespace-pre-line não funcionou*/}
+            <h1 className="mx-60 mt-20 mb-8 text-right text-5xl text-brancoBX"> Etapas <br></br> <span className="text-laranjaBX">&</span> Desafios</h1> 
             <div className="flex items-center flex-col">
                 <section className="mx-10 my-8 flex flex-wrap justify-center gap-x-7 gap-y-7">
                     {
                         etapasDesafios.map((etapa) => (
-                            <button onClick={() => handleOnClick(etapa.etapaIndice)} disabled={etapa.estado == false} title={`Etapa ${etapa.etapaIndice}`}
-                                className={`relative size-44 rounded-3xl ${etapa.estado == true ? 'hover:transition-all hover:duration-150 hover:underline hover:underline-offset-4 hover:decoration-verdeBX hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:bg-blue-600 hover:bg-opacity-20 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''} ${indice == etapa.etapaIndice ? 'scale-[1.13] shadow-sky-900 shadow-lg ' : ''}`}>
-                                {/*gostaria de adicionar um hover que escale apenas os elementos textuais/headers*/}
+                            //o clique só é viável para as etapas liberadas, com isso os hovers, etc, também só devem ser aplicados aos Cards cujas etapas já foram liberadas, em caso contrário nada deve ser feito
+                            <button onClick={() => handleOnClick(etapa.etapaIndice)} disabled={etapa.estado == false} title={`Etapa ${etapa.etapaIndice}`} className={`relative size-44 rounded-3xl ${etapa.estado == true ? 'hover:transition-all hover:duration-150 hover:underline hover:underline-offset-4 hover:decoration-verdeBX hover:ring-8 hover:ring-azulBX hover:ring-opacity-20 hover:bg-blue-600 hover:bg-opacity-20 active:ring-8 active:ring-laranjaBX active:ring-opacity-70' : ''}  ${indice == etapa.etapaIndice ? 'scale-[1.13] shadow-sky-900 shadow-lg ' : ''}`}>
                                 {
-                                    etapa.estado == false ?
-                                        <div>
-                                            <ImageG className="rounded-3xl" src="/staticTV.gif" alt="TV estática" fill={true} />
+                                    etapa.estado == false ? //caso a etapa ainda não tenha ocorrido:
+                                        <div> 
+                                            <ImageG className="rounded-3xl" src="/staticTV.gif" alt="TV estática" fill={true} /> 
                                         </div>
-                                        :
+                                        : //caso contrário (etapa.estado==true):
                                         <div>
                                             <ImageG className="-z-10 rounded-3xl opacity-[0.95]" src={etapa.icon} alt={etapa.altIcon} fill={true} />
                                             <h2 className={`mt-24 ${poppins.className} text-white text-2xl drop-shadow-lg underline underline-offset-4 decoration-verdeBX`}><b>Etapa {etapa.etapaIndice}</b></h2>
@@ -52,10 +51,10 @@ export default function EtapasDesafios() {
                             </button>
                         ))
                     }
-                </section>
-                <section id="scrollDestiny" className="pt-8 mb-20 mx-8"> {/*a fim do scroll ter como destino um ponto ligeiramente anterior ao início do primeiro balao de desafio, ao invés de utilizar mt-8, aqui usamos pt-8 */}
+                </section> 
+                <section id="scrollDestiny" className="pt-8 mb-20 mx-8"> {/*a fim do scroll ter como destino um ponto ligeiramente anterior ao início do primeiro balao de desafio, ao invés de utilizar mt-8, aqui usamos pt-8 */} 
                     {
-                        indice != -1 && (
+                        indice != -1 && ( //garantindo que os CardDesafios só serão chamados caso haja um valor válido em 'indice', evitando tentativas de acesso à posições não existentes no arranjo de etapas
                             <CardDesafios desafios={(etapasDesafios[indice]).desafios} /> 
                         )
                     }
