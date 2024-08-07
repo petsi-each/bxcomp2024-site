fcdc1e90f1461dfe86ddfb12cfb293c9db715bfc
Daniyutak <dtakara1101@gmail.com>
2024-08-07 11:54:07 -0300

    arruma a interatividade do botão e o map dos botões

--- src/components/BotaoEquipe.tsx
+++ src/components/BotaoEquipe.tsx
@@ -2,19 +2,28 @@
 
 import ImageG from "@/components/ImageG"
 import equipes_data from '@/data/landingpage.json'
+import { MouseEventHandler } from "react";
+
+interface ObjEquipe{
+    nome: string;
+    membros: string[];
+    pontos: number;
+    iconPath: string;
+}
 
 interface BotaoEquipeProps{
-    nomeEquipe: string;
-    srcImage: string;
+    equipe: ObjEquipe,
+    selecionado: boolean,
+    onClick: MouseEventHandler
 }
 
 export default function BotaoEquipe(props: BotaoEquipeProps){
     return(
         
-        <button className="w-20 h-28 opacity-50 px-4 py-4 mx-1 my-2 rounded-md border border-slate-500 bg-brancoBX focus:bg-laranjaBX focus:opacity-100 focus:drop-shadow-sm">
-            <ImageG src={props.srcImage} width={32} height={32} alt={`ícone da equipe ${props.nomeEquipe}`}></ImageG>
+        <button onClick={props.onClick} className="w-20 h-28 opacity-50 px-4 py-4 mx-1 my-2 rounded-md border border-slate-500 bg-brancoBX ">
+            <ImageG src={props.equipe.iconPath} width={32} height={32} alt={`ícone da equipe ${props.equipe.nome}`}></ImageG>
             <div className="text-pretoBX pt-2">
-                {props.nomeEquipe}
+                {props.equipe.nome}
             </div>
         </button>
 
--- src/components/Equipes.tsx
+++ src/components/Equipes.tsx
@@ -5,11 +5,13 @@ import { Poppins } from 'next/font/google'
 import { Lilita_One } from 'next/font/google'
 import equipes_data from '@/data/landingpage.json'
 import BotaoEquipe from "./BotaoEquipe"
+import { MouseEvent, useState } from "react"
 
 const poppins = Poppins({weight: "300", subsets: ['latin']})
 
-interface Equipe {
 const lilita = Lilita_One({weight: "400", subsets: ['latin']})
+
+/* interface Equipe {
     nome: string,
     srcImage: string,
     membros: string[],
@@ -22,38 +24,34 @@ const equipes = equipes_data.equipes.map((equipe, index) => {
     membros: equipe.membros
     };
   });
+*/
 
 export default function Equipes(){
+    const [idSelecionado, setIdSelecionado] = useState(0)
+    
     return (
-    <section className="w-auto py-12 px-16 bg-brancoBX flex flex-row justify-center padding">
-        <section className="px-4 py-2 justify-center flex-row items-center">
-            <section className="px-2 flex col items-center">
-            <BotaoEquipe 
-            nomeEquipe={equipes_data.equipes[0].nome}
-                <div>
-                {equipes.map( (equipe, index) => {
-                    return (
-                    <div key={index}>
-                        <BotaoEquipe nomeEquipe={equipes.nome} srcImage={equipes.srcImage}/>
-                    </div>
-                    )
-                    })
+    <section className="py-12 px-16 bg-brancoBX grid grid-cols-1 md:grid-cols-2 justify-center padding">
+        <section className="px-4 py-2 flex flex-wrap items-center justify-center ">
+            <section className="h-fit flex flex-wrap justify-center">
+                { equipes_data.equipes.map((equipe, idx) => {
+                    return <BotaoEquipe key={idx} equipe={equipe} selecionado={idSelecionado==idx} onClick={()=> setIdSelecionado(idx)} />  
+                })
                 }
-                </div>
-                
+
             </section>
         </section>
 
         <section className=" px-4 flex flex-col items-center justify-center">
             <h2 className="text-verdeBX text-6xl"> Equipes </h2>
             <p className= {`${poppins.className} py-4 text-sm text-pretoBX`}> Conheça os competidores desse ano! </p>
-            <section className="relative">
+
+            <section className=" relative">
                 <div className="z-10 relative border-laranjaBX border-8 rounded-full bg-white w-72 h-72 drop-shadow-lg"></div>
                 <div className="z-0 absolute bottom-20 -right-20  bg-verdeBX text-white rounded-full pl-8 pr-6 py-2 drop-shadow-md">   12 pontos </div>
             </section>
-
+            
             <div className="z-20 -mt-16 drop-shadow-md bg-laranjaBX w-80 h-36 rounded-tl-[96px] rounded-br-[96px] flex flex-col items-center justify-center">
-                <h3 className= {`${lilita.className} my-2 tracking-wide drop-shadow-md text-3xl font-bold`}> Nome Equipe </h3>
+                <h3 className= {`${lilita.className} my-2 tracking-wide drop-shadow-md text-3xl font-bold`}> {equipes_data.equipes[idSelecionado].nome} </h3>
                 <div className= {`${poppins.className} text-xs text-white flex flex-col items-center justify-center`}>
                     <p>Nome</p>
                     <p>Nome</p>
@@ -61,8 +59,6 @@ export default function Equipes(){
                     <p>Nome</p>
                 </div>
             </div>
-            
-            
 
         </section>
     </section>
