c8dc0acc15a2947c41cc61aee86cfd971909b58a
LumenRuby <lucaspanta007@gmail.com>
2024-08-10 13:53:21 -0300

    fix: reduzed border radius on mobile, less space between components on mobile, code cleanup

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,8 +3,8 @@ import ImageG from "@/components/ImageG";
 
 export default function Hero() {
     return (
-        <section className="flex items-center justify-center relative overflow-hidden min-h-[calc(100vh-74px)] pb-16 px-4 md:px-8 lg:px-16">
-            <div className="w-full max-w-3xl flex justify-center">
+        <section className="flex items-center justify-center overflow-visible relative md:min-h-[calc(100vh-74px)] pb-8 md:pb-12 lg:pb-16 px-4 md:px-8 lg:px-16">
+            <div className="w-full max-w-3xl flex justify-center transform translate-y-28 md:translate-y-0">
                 <ImageG 
                     className="w-full h-auto" 
                     src="/home/logobxcomp.png" 
--- src/app/components/oqueeobxcomp.tsx
+++ src/app/components/oqueeobxcomp.tsx
@@ -13,16 +13,16 @@ const poppins = Poppins({
 
 export default function oqueeobxcomp() {
     return (
-        <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-[pretoBX]">
+        <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-[pretoBX] md:pb-16 pt-8 md:pt-12 lg:pt-16">
             {/* Background Image Container */}
             <div className="absolute inset-0 z-0 flex items-center justify-center">
-                <ImageG src="/home/sobrebx.png" alt="Logo do BXComp" quality={100} width={1154} height={875} className="object-contain"/>
+                <ImageG src="/home/sobrebx.png" quality={100} width={1154} height={875} className="object-contain" alt={""}/>
             </div>
 
             {/* Text Balloon */}
-            <div className="relative z-10 w-full max-w-sm md:max-w-2xl p-8 md:p-16 lg:p-24 bg-brancoBX bg-opacity-90 rounded-[50px] md:rounded-[100px] lg:rounded-[125px] shadow-lg text-center flex flex-col items-center mx-4">
+            <div className="relative z-10 w-full max-w-sm md:max-w-2xl p-8 md:p-16 lg:p-24 bg-brancoBX bg-opacity-90 rounded-[25px] md:rounded-[100px] lg:rounded-[125px] shadow-lg text-center flex flex-col items-center mx-4">
                 <h2 className="text-2xl md:text-3xl lg:text-4xl text-laranjaBX mb-4">O que é BXCOMP?</h2>
-                <p className={`${poppins.className} text-sm md:text-base lg:text-lg text-black mb-6 font-pop text-left`}>
+                <p className={`${poppins.className} text-sm md:text-base lg:text-lg text-black mb-6 text-left`}>
                     O <span className="font-bold">BXComp</span> é o Campeonato de Programação para Calouros do Curso de Sistemas de Informação organizado pelo PET-SI. Seu objetivo é criar um ambiente divertido e desafiador, composto por atividades de resolução de problemas usando programação.
                 </p>
                 <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">                   
