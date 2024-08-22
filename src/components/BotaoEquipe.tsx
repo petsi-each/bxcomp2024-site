9aeefad3f17ea482a886a1c0f1ea51250208cf1d
Daniyutak <dtakara1101@gmail.com>
2024-08-22 15:05:30 -0300

    fix:comenta e organiza o código e muda arialabel de botao equipes

--- src/components/BotaoEquipe.tsx
+++ src/components/BotaoEquipe.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import ImageG from "@/components/ImageG"
-import equipes_data from '@/data/landingpage.json'
 import { MouseEventHandler } from "react";
 
 interface ObjEquipe{
@@ -20,7 +19,7 @@ interface BotaoEquipeProps{
 export default function BotaoEquipe(props: BotaoEquipeProps){
     return(
         
-        <button aria-label="mostrar equipe" onClick={props.onClick} className={`flex flex-col items-center justify-center w-20 py-2 px-2 h-26 mx-1 my-2 rounded-md border border-slate-500
+        <button aria-label={`mostrar equipe ${props.equipe.nome}`} onClick={props.onClick} className={`flex flex-col items-center justify-center w-20 py-2 px-2 h-26 mx-1 my-2 rounded-md border border-slate-500
          ${props.selecionado ? 'bg-laranjaBX' : 'opacity-50 bg-brancoBX  hover:bg-zinc-300 hover:opacity-75  '}`}>
             <ImageG className="w-16" src={props.equipe.iconPath} width={60} height={60} alt=""></ImageG>
         <div className="w-16 truncate text-center text-pretoBX p-1"> {props.equipe.nome} </div>
