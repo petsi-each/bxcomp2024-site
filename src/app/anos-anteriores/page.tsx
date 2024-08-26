8d059040a4c82219859270a91d487d05d5dd4ae7
pinheiro-lu <luanpereira0603@gmail.com>
2024-08-26 19:34:09 -0300

    feat: add structure for timeline of anos anteriores page

--- src/app/anos-anteriores/page.tsx
+++ src/app/anos-anteriores/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Poppins } from 'next/font/google';
+import { PiCaretLeftFill } from "react-icons/pi";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -19,6 +20,39 @@ export default function AnosAnterioresPage() {
         </div>
         <Image src='/anos-anteriores/fotosAnosAnteriores.png' alt='Três fotos de edições anteriores do BXComp. Com mais destaque, ao centro, participantes e organizadores em torno de um banner do BXComp. À esquerda, com parte da imagem atrás da central, participantes durante a competição no laboratório de informática. À direita, também com parte da imagem atrás da central, quatro membros de uma equipe vencedora segurando sacolas de prêmios.' width={818} height={360} />
         <p>Conheça um pouco dessa história e se prepare para a nova edição do BXCOMP visitando os sites das antigas edições.</p>
+        <section className='space-y-8 relative'>
+          <div className='absolute top-8 left-2 -z-10 bottom-0 w-0.5 bg-verdeBX'></div>
+          <article>
+            <div className='flex items-center '>
+              <div className='w-4 h-4 bg-white rounded-full'></div>
+              <figure className='flex items-center'>
+                <PiCaretLeftFill className='-me-1.5' />
+                <div className='bg-white w-[263px] h-[76px] rounded-xl flex justify-center items-center '>
+                  <Image src='/anos-anteriores/logoBx2022.svg' alt='Logo da 12ª edição do BXComp' width={204} height={45} />
+                </div>
+              </figure>
+            </div>
+            <div className='text-right'>
+              <p>2022 - 12ª edição</p>
+              <p className='text-xs'>Brasilidades</p>
+            </div>
+          </article>
+          <article>
+            <div className='flex items-center '>
+              <div className='w-4 h-4 bg-white rounded-full'></div>
+              <figure className='flex items-center'>
+                <PiCaretLeftFill className='-me-1.5' />
+                <div className='bg-white w-[263px] h-[76px] rounded-xl flex justify-center items-center '>
+                  <Image src='/anos-anteriores/logoBx2022.svg' alt='Logo da 12ª edição do BXComp' width={204} height={45} />
+                </div>
+              </figure>
+            </div>
+            <div className='text-right'>
+              <p>2022 - 12ª edição</p>
+              <p className='text-xs'>Brasilidades</p>
+            </div>
+          </article>
+        </section>
       </div>
     </main>
   );
