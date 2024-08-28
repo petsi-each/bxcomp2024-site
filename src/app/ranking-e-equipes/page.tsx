2200c9d8c9830a3fcbc2512a7755b6d0cb853e40
jun <gustavonagatomo@gmail.com>
2024-08-28 11:54:43 -0300

    fix: Estica e remove bordinha da imagem da onda

--- src/app/ranking-e-equipes/page.tsx
+++ src/app/ranking-e-equipes/page.tsx
@@ -6,9 +6,9 @@ import ImageG from '@/components/ImageG'
 export default function RankingEEquipes() {
     return (
         <main className='pt-24'>
-            <ImageG className='pointer-events-none rotate-180' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
+            <ImageG className='w-full -mb-1 pointer-events-none rotate-180' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Equipes />
-            <ImageG className='pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
+            <ImageG className='w-full -mt-1 pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Ranking equipes={data.equipes} />
         </main>
     )
