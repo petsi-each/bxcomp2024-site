87e852e6738a19f4d5932e2d7a9b4ff79749472e
jun <gustavonagatomo@gmail.com>
2024-08-28 01:15:53 -0300

    feat: Adiciona ondinhas

--- src/app/ranking-e-equipes/page.tsx
+++ src/app/ranking-e-equipes/page.tsx
@@ -1,11 +1,14 @@
 import Ranking from '@/components/Ranking'
 import Equipes from '@/components/Equipes'
 import data from '@/data/landingpage.json'
+import ImageG from '@/components/ImageG'
 
 export default function RankingEEquipes() {
     return (
-        <main className='pt-36'>
+        <main className='pt-24'>
+            <ImageG className='pointer-events-none rotate-180' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Equipes />
+            <ImageG className='pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Ranking equipes={data.equipes} />
         </main>
     )
