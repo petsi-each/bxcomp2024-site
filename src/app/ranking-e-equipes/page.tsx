3bd068f104d518bfca62806f34d1f384dcaa3320
jun <gustavonagatomo@gmail.com>
2024-08-28 13:38:02 -0300

    feat: Estiliza ranking tal como a home

--- src/app/ranking-e-equipes/page.tsx
+++ src/app/ranking-e-equipes/page.tsx
@@ -5,11 +5,14 @@ import ImageG from '@/components/ImageG'
 
 export default function RankingEEquipes() {
     return (
-        <main className='pt-24'>
+        <main className='pt-24'> {/*Esse padding faz com que parte da onda fique atrás da navbar. Porém, deixar maior empurra o conteúdo muito para baixo.*/}
             <ImageG className='w-full -mb-1 pointer-events-none rotate-180' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Equipes />
             <ImageG className='w-full -mt-1 pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
-            <Ranking equipes={data.equipes} />
+            <section className="flex flex-col my-12 lg:px-16 gap-12">
+                <h1 className="text-7xl px-8">Ranking</h1>
+                <Ranking equipes={data.equipes} />
+            </section>
         </main>
     )
 }
