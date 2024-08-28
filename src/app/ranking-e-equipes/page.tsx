343e7e20a52f07f0be87c3085d77cfaa471531f2
jun <gustavonagatomo@gmail.com>
2024-08-28 14:00:16 -0300

    feat: Adiciona âncora de HTML para o ranking.

--- src/app/ranking-e-equipes/page.tsx
+++ src/app/ranking-e-equipes/page.tsx
@@ -8,7 +8,8 @@ export default function RankingEEquipes() {
         <main className='pt-24'> {/*Esse padding faz com que parte da onda fique atrás da navbar. Porém, deixar maior empurra o conteúdo muito para baixo.*/}
             <ImageG className='w-full -mb-1 pointer-events-none rotate-180' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Equipes />
-            <ImageG className='w-full -mt-1 pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
+            {/*OBS: A âncora do ranking (id="ranking") está em ImageG porque se deixar na própria section do ranking ele fica escondido atrás da navbar. É uma gambiarra mas funciona bem nesse caso. A outra alternativa seria fazer uma função dedicada para aplicar esse offset.*/}
+            <ImageG id="ranking" className='w-full -mt-1 pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <section className="flex flex-col my-12 lg:px-16 gap-12">
                 <h1 className="text-7xl px-8">Ranking</h1>
                 <Ranking equipes={data.equipes} />
