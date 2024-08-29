8d092af2ed6f5df6cf593b7215df16114706476c
Daniyutak <dtakara1101@gmail.com>
2024-08-29 04:55:14 -0300

    muda o caminho so p ter ctz que vai usar o png certo
    nao sei se era minha cache bugando mas nao aparecia mudança

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <div className="w-full max-w-3xl flex justify-center mt-24 md:mt-4">
                 <ImageG 
                     className="w-full h-auto" 
-                    src="/home/logobxcomp.png" 
+                    src="/logoBxcomp.png" 
                     alt="Logo do BXCOMP"
                     width={725} 
                     height={490} 
