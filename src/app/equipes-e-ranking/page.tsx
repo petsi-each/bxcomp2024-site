f0704dcb2abc9ab17a7669824abb56a1a451502f
jun <gustavonagatomo@gmail.com>
2024-08-29 11:21:43 -0300

    chore: Remove do código comentários de decisões tomadas

--- src/app/equipes-e-ranking/page.tsx
+++ src/app/equipes-e-ranking/page.tsx
@@ -5,10 +5,9 @@ import ImageG from '@/components/ImageG'
 
 export default function EquipesERanking() {
     return (
-        <main className='pt-24'> {/*Esse padding faz com que parte da onda fique atrás da navbar. Porém, deixar maior empurra o conteúdo muito para baixo.*/}
+        <main className='pt-24'>
             <ImageG className='w-full -mb-1 pointer-events-none rotate-180' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <Equipes />
-            {/*OBS: A âncora do ranking (id="ranking") está em ImageG porque se deixar na própria section do ranking ele fica escondido atrás da navbar. É uma gambiarra mas funciona bem nesse caso. A outra alternativa seria fazer uma função dedicada para aplicar esse offset.*/}
             <ImageG id="ranking" className='w-full -mt-1 pointer-events-none' src='home/onda_branca.svg' width="1280" height="186" alt='Onda branca' />
             <section className="flex flex-col my-12 lg:px-16 gap-12">
                 <h1 className="text-7xl px-8">Ranking</h1>
