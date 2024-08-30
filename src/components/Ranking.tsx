8b319b38c76b2198b0abad341978b5728b2bb950
Giane Mayumi <62656745+Anemaygi@users.noreply.github.com>
2024-08-30 06:22:05 +0000

    fix: percentage

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -68,6 +68,8 @@ const PointBar: React.FC<PointBarProps> = ({ equipe, topScore, scorePosition }) 
     
     const percentage = (totalPontos / topScore) * 100
 
+    const size = percentage < 8 ? 'fit-content' :  percentage + '%'
+
     return (
 
         <article className="-z-10">
@@ -78,8 +80,8 @@ const PointBar: React.FC<PointBarProps> = ({ equipe, topScore, scorePosition }) 
                 <p className="text-md">{totalPontos} pontos</p>
             </div>
 
-
-            <div style={{ width: percentage + '%' }} className={`flex items-center px-8`}>
+            <div className="w-full px-8 flex">
+            <div style={{ width: size }} className={`flex items-center bg-red-500`}>
 
                 <div className="flex flex-grow h-10 drop-shadow-lg bg-white rounded-full">
                     <div className="w-full drop-shadow-lg rounded-full m-2 h-6 bg-laranjaBX"></div>
@@ -93,6 +95,8 @@ const PointBar: React.FC<PointBarProps> = ({ equipe, topScore, scorePosition }) 
                 </div>
 
                 {/* Descrição da equipe no desktop */}
+                
+            </div>
                 <div className="px-4 max-w-10 min-w-10 md:max-w-28 md:min-w-28 hidden md:block">
                     <h2 className="text-2xl w-full truncate">{equipe.nome}</h2>
                     <p className="text-md w-full">{totalPontos} pontos</p>
