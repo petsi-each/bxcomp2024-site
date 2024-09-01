6836c417c737f9515673b61c5f1daacea14149e7
Daniyutak <dtakara1101@gmail.com>
2024-09-01 19:35:43 -0300

    fix responsivity for home page

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,13 +4,13 @@ import ImageG from "@/components/ImageG";
 export default function Hero() {
     return (
         <section className="flex items-center justify-center overflow-visible relative md:min-h-screen px-4 md:px-8 lg:px-16">
-            <div className="w-full max-w-3xl flex justify-center mt-24 md:mt-4">
+            <div className="max-w-3xl flex justify-center mt-24 md:mt-4">
                 <ImageG 
-                    className="w-full h-auto scale-110 sm:scale-150" 
+                    className="w-full sm:w-96 md:w-4/5 lg:w-full h-auto scale-110 sm:scale-150" 
                     src="/logoBxcomp.svg" 
                     alt="Logo do BXCOMP"
-                    width={1400} 
-                    height={1000} 
+                    width={875} 
+                    height={625} 
                 />
             </div>
         </section>
