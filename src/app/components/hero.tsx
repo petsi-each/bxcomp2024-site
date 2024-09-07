ded2e81e2784bee5244b75455921f63616a5bb9d
Daniyutak <dtakara1101@gmail.com>
2024-09-07 18:10:08 -0300

    fix: pagina quebrando
    mas agora a imagem fica minuscula no celular

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,14 +3,13 @@ import ImageG from "@/components/ImageG";
 
 export default function Hero() {
     return (
-        <section className="flex items-center justify-center overflow-visible relative md:min-h-screen px-4 md:px-8 lg:px-16">
-            <div className="max-w-3xl flex justify-center mt-24 md:mt-4">
+        <section className="flex items-center justify-center relative md:min-h-screen px-4 md:px-8 lg:px-16">
+            <div className="w-full flex overflow-x-hidden justify-center mt-24 md:mt-4">
                 <ImageG 
-                    className="w-full sm:w-96 md:w-4/5 lg:w-full h-auto scale-110 sm:scale-150" 
+                    className="w-full h-auto scale-110" 
                     src="/logoBxcomp.svg" 
                     alt="Logo do BXCOMP"
-                    width={875} 
-                    height={625} 
+                    fill={true}
                 />
             </div>
         </section>
