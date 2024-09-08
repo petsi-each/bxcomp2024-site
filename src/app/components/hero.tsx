ab1dc1867da8b4461f5e997b1d9d1eea53a4fe0b
Daniyutak <dtakara1101@gmail.com>
2024-09-08 00:22:14 -0300

    feat: adiciona setinha animada

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,17 +1,25 @@
 'use client'
 import ImageG from "@/components/ImageG";
-
+import { VscArrowCircleDown } from "react-icons/vsc";
 export default function Hero() {
     return (
-        <section className="flex items-center justify-center relative md:min-h-screen px-4 md:px-8 lg:px-16">
+        <section className="flex flex-col items-center justify-center relative md:min-h-screen px-4 md:px-8 lg:px-16">
+            
             <div className="w-full flex h-60 overflow-x-hidden justify-center object-center mt-24 md:mt-0">
                 <ImageG 
-                    className="w-full h-auto pr-8" 
+                    className="w-full h-auto static pr-6 pb-36" 
                     src="/logoBxcomp.svg" 
                     alt="Logo do BXCOMP"
                     fill={true}
                 />
             </div>
+
+            <div className="block pt-20">
+            <VscArrowCircleDown className="text-brancoBX w-16 h-16 animate-bounce justify-center" />
+            </div>
+
         </section>
+
+
     );
 }
