f872572f78e37bdac4ed1107779175c4e21e8a39
Daniyutak <dtakara1101@gmail.com>
2024-09-07 23:31:39 -0300

    fix: tamanho da imagem para menor q md

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,9 +4,9 @@ import ImageG from "@/components/ImageG";
 export default function Hero() {
     return (
         <section className="flex items-center justify-center relative md:min-h-screen px-4 md:px-8 lg:px-16">
-            <div className="w-full flex overflow-x-hidden justify-center mt-24 md:mt-4">
+            <div className="w-full flex h-60 overflow-x-hidden justify-center object-center mt-24 md:mt-0">
                 <ImageG 
-                    className="w-full h-auto scale-110" 
+                    className="w-full h-auto pr-8" 
                     src="/logoBxcomp.svg" 
                     alt="Logo do BXCOMP"
                     fill={true}
