0a07c83ec55edda2f0942a143ccc2bf86fa54a6f
Daniyutak <dtakara1101@gmail.com>
2024-09-12 20:46:53 -0300

    Ajustes finais na pagina

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,20 +3,16 @@ import ImageG from "@/components/ImageG";
 import { VscArrowCircleDown } from "react-icons/vsc";
 export default function Hero() {
     return (
-        <section className="flex flex-col items-center justify-center relative md:min-h-screen px-4 md:px-8 lg:px-16">
-            
-            <div className="w-full flex h-60 overflow-x-hidden justify-center object-center mt-24 md:mt-0">
+        <section className="flex -mt-24 items-center justify-center relative h-screen">
+
                 <ImageG 
-                    className="w-full h-auto static pr-6 pb-36" 
+                    className="" 
                     src="/logoBxcomp.svg" 
                     alt="Logo do BXCOMP"
                     fill={true}
                 />
-            </div>
 
-            <div className="block pt-20">
-            <VscArrowCircleDown className="text-brancoBX w-16 h-16 animate-bounce justify-center" />
-            </div>
+            <VscArrowCircleDown className="text-brancoBX w-10 h-10 absolute bottom-2 md:w-16 md:h-16 animate-bounce justify-center" />
 
         </section>
 
